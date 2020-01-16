e5b83406ba88eaf951815b0dd0b4ecc674719714
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-16 14:38:46 +0000

    Passed.2 - added functionality

--- lib/diary.rb
+++ lib/diary.rb
@@ -1,4 +1,30 @@
+require 'pg'
+
 
 class Diary
+
+  attr_reader :id, :title, :body
+
+
+  def initialize(id:, title:, body:)
+    @id = id
+    @title = title
+    @body = body 
+  end
+
+
+
+  def self.add(title:, body:)
+    if ENV['DATABASE'] =='test'
+      connect = PG.connect(:dbname => 'diary_manager_test')
+    else
+      connect = PG.connect(:dbname => 'diary_manager')
+    end
+
+    result = connect.exec("INSERT INTO diary_note (note_title,note_body) VALUES('#{title}','#{body}') RETURNING id, note_title, note_body;")
+
+    Diary.new(id: result[0]['id'], title: result[0]['note_title'], body: result[0]['note_body'])
+
+  end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 ENV['RACK_ENV'] = 'test'
+ENV['DATABASE'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
