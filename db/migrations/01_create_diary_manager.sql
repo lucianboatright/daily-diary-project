640d8b05ec2ce1e91079df18214d2b04480ee2d7
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-16 12:09:41 +0000

    Test.1 feature test for add not title and body

---
+++ db/migrations/01_create_diary_manager.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE diary_manager;
+
+CREATE TABLE diary_note(id SERIAL PRIMARY KEY, note_title VARCHAR(20), note_body VARCHAR(250));
+
+
+CREATE DATABASE diary_manager_test;
+
+CREATE TABLE diary_note(id SERIAL PRIMARY KEY, note_title VARCHAR(20), note_body VARCHAR(250));
---
+++ spec/features/note_entry_spec.rb
@@ -0,0 +1,9 @@
+feature 'title_page' do
+  scenario 'check title and text was added' do
+    visit '/'
+    fill_in :title, with: 'first note'
+    fill_in :body, with: 'text body'
+    click_button 'Submit'
+    expect(page).to have_content ('note added to diary')
+  end
+end
